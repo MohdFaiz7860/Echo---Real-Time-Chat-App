3bc0e627187d1db643d0c0e18d5c1055bada3a4f
Mohd Faiz <mohdfaiz.3950@gmail.com>
2026-02-24 15:07:54 +0530

    Update BASE_URL for development environment

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "https://echo-real-time-chat-app-backend.onrender.com" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "https://echo-real-time-chat-app-backend-pp0p.onrender.com" : "/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
